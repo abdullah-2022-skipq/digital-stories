7562edaca123664a3f607de4439aff85bd3ee8e4
Abdullah <abdullah.tahir.skipq@gmail.com>
2022-12-21 19:11:44 +0500

    add mongoose model for user

---
+++ backend/models/user.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema, "users");
