fb4a93f18346384c692cec2e2cea499bacad1ece
Abdullah <abdullah.tahir.skipq@gmail.com>
2022-12-23 21:46:38 +0500

    add story schema

---
+++ backend/models/story.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const storySchema = new Schema(
+  {
+    caption: { type: String, required: true, default: "" },
+    image: { type: String, default: "" },
+    video: { type: String, default: "" },
+    bgColor: { type: Number, required: true, default: 0 },
+    font: { type: Number, required: true, default: 0 },
+    upVoteCount: { type: Number, default: 0 },
+    downVoteCount: { type: Number, default: 0 },
+    commentCount: { type: Number, default: 0 },
+    postedBy: { type: mongoose.SchemaTypes.ObjectId, ref: "users" },
+  },
+  { timestamps: true }
+);
+
+storySchema.virtual("trendingScore").get(function () {
+  return `${this.upVotes} + ${this.downVotes} + ${this.commentCount}`;
+});
+
+// post
+
+export default mongoose.model("Story", storySchema, "stories");
