485664af11f57b4471b7a7e4f92da55cbf4cea10
Abdullah <abdullah.tahir.skipq@gmail.com>
2022-12-23 21:51:13 +0500

    add display picture in user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -8,6 +8,8 @@ const userSchema = new Schema(
     username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
+    //! multer -> save -> name allot -> path -> TODO middleware
+    displayPicPath: { type: String, default: "" },
   },
   { timestamps: true }
 );
