38cb82fd65eff232ec7c7577b2959c162335e8d0
Abdullah <abdullah.tahir.skipq@gmail.com>
2022-12-27 21:12:10 +0500

    add custom error

--- backend/services/CustomErrorHandler.js
+++ backend/services/CustomErrorHandler.js
@@ -12,6 +12,14 @@ class CustomErrorHandler extends Error {
   static wrongCredentials(message = "Email or password is wrong!") {
     return new CustomErrorHandler(401, message);
   }
+
+  static unAuthorized(message = "Unauthorized") {
+    return new CustomErrorHandler(401, message);
+  }
+
+  static notFound(message = "Not found") {
+    return new CustomErrorHandler(404, message);
+  }
 }
 
 export default CustomErrorHandler;
