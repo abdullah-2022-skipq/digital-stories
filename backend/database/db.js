62b3e7d87a01f36dc342f19b97f3f00f67a8b56d
Abdullah <abdullah.tahir.skipq@gmail.com>
2022-12-28 22:36:09 +0500

    add db code

---
+++ backend/database/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { MONGO_DB_LOCAL } from "../config";
+
+const dbConnect = () => {
+  mongoose.connect(MONGO_DB_LOCAL);
+  const db = mongoose.connection;
+
+  db.on("error", console.error.bind(console, "connection error: "));
+
+  db.once("open", () => {
+    console.log(`😄 Database connected on server ${MONGO_DB_LOCAL}`);
+  });
+};
+
+export default dbConnect;
