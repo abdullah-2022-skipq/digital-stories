e2cfc6fe515ab63c3b44a3c9b4950555896eeb82
Abdullah <abdullah.tahir.skipq@gmail.com>
2022-12-28 22:35:46 +0500

    refactor database code

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,7 @@
 import express from "express";
-import { PORT, MONGO_DB_LOCAL } from "./config";
+import { PORT } from "./config";
 import router from "./routes/index";
 import { errorHandler } from "./middlewares/";
-
-const mongoose = require("mongoose");
-
-const PATH = "mongodb://127.0.0.1:27017/digital-stories";
-
-mongoose.connect(PATH);
-
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", () => {
-  console.log(`Database connected on server ${PATH} 😄`);
-});
 
 const app = express();
 app.use(express.json());
