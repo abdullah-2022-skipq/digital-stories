37b3d716369b378f5f8bd6bd9d88f4470ad48542
Abdullah <abdullah.tahir.skipq@gmail.com>
2022-12-28 22:36:31 +0500

    add routes

---
+++ backend/routes/routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  registerController,
+  loginController,
+  userController,
+} from "../controllers/";
+
+import { auth } from "../middlewares";
+
+const router = express.Router();
+
+router.post("/register", registerController.register);
+router.post("/login", loginController.login);
+router.get("/me", auth, userController.myDetails);
+
+export default router;
