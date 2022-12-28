6434bd8a2f46aa71a360d94edca478a4b28bc077
Abdullah <abdullah.tahir.skipq@gmail.com>
2022-12-28 22:36:00 +0500

    refactor server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 import express from "express";
 import { PORT } from "./config";
-import router from "./routes/index";
+import { router } from "./routes/index";
 import { errorHandler } from "./middlewares/";
+import { dbConnect } from "./database";
+
+dbConnect();
 
 const app = express();
+
 app.use(express.json());
 
 app.use("/api", router);
 
 app.use(errorHandler);
+
 app.listen(PORT, () => {
-  console.log(`Backend Server is up and running on port ${PORT} 🚀`);
+  console.log(`🚀 Backend Server is up and running on port ${PORT}`);
 });
