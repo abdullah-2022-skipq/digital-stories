def0ce645b6ce49e9780309b6e2b3236d018eb8b
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2022-12-31 23:01:58 +0500

    re-structure schemas

---
+++ frontend/src/schemas/index.js
@@ -0,0 +1,6 @@
+import { contactDetailsSchema } from "./ContactDetailsSchema";
+import { usernameSchema } from "./usernameSchema";
+import { passwordSchema } from "./passwordSchema";
+import { signInSchema } from "./signInSchema";
+
+export { contactDetailsSchema, usernameSchema, passwordSchema, signInSchema };
