036a3cf132de180c5dfa46a54a1523701ce306fe
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2022-12-31 22:56:20 +0500

    fix cors settings

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,18 @@ import { PORT } from "./config";
 import { router } from "./routes/index";
 import { errorHandler } from "./middlewares/";
 import { dbConnect } from "./database";
+import cors from "cors";
 
 dbConnect();
 
 const app = express();
+
+const corsOptions = {
+  credentials: true,
+  origin: ["http://localhost:5173"],
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
