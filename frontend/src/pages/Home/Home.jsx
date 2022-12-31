e9a802f682891885e8289ae49c311a7618f20a0a
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2022-12-31 23:00:58 +0500

    add Home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,37 +1,7 @@
-import { Link, useHistory } from "react-router-dom";
-import styles from "./Home.module.css";
-import Card from "../../components/shared/Card/Card";
-import Button from "../../components/shared/Button/Button";
-import { signInStyle } from "../../shared/styles";
+import React from "react";
 
 const Home = () => {
-  const history = useHistory();
-
-  const getStartedHandler = () => {
-    history.push("/get-started");
-  };
-
-  return (
-    <div className="cardWrapper">
-      <Card cardHeading="Welcome" cardLogo="waving_hand">
-        <p className={styles.description}>
-          Digital Stories – the newest platform for sharing and discovering
-          stories from around the world.
-          <span className={styles.tagLine}> Snap your story now!</span>
-        </p>
-
-        <div>
-          <Button onClick={getStartedHandler} buttonTitle="Let's Get Started" />
-          <div className="hasAccountAlreadyWrapper">
-            <span className="hasAccountAlready">Have an account already?</span>
-            <Link style={signInStyle} to="/sign-in">
-              Sign in
-            </Link>
-          </div>
-        </div>
-      </Card>
-    </div>
-  );
+  return <div>Home Page - home to all the stories</div>;
 };
 
 export default Home;
