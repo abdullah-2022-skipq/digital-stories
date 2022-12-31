597807ad7a8ab5c6c7ce742bb7f11f162dc382e0
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2022-12-31 22:56:31 +0500

    fix imports

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,11 +1,5 @@
 import registerController from "./auth/registerController";
 import loginController from "./auth/loginController";
-import createStoryController from "./story/createStoryController";
 import userController from "./user/userController";
 
-export {
-  registerController,
-  loginController,
-  userController,
-  createStoryController,
-};
+export { registerController, loginController, userController };
