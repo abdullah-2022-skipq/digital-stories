cedc9b6bf9b26f705996abf106f352bdd2ef31ae
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-01 13:12:55 +0500

    add mult-step-form slice

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import auth from "./authSlice";
 import userRegistration from "./userRegistrationSlice";
+import multiStepForm from "./multiStepFormSlice";
 
-export const store = configureStore({ reducer: { auth, userRegistration } });
+export const store = configureStore({
+  reducer: { auth, userRegistration, multiStepForm },
+});
---
+++ frontend/src/store/multiStepFormSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+import StepContactDetails from "../pages/MultiStepForm/StepContactDetails/StepContactDetails";
+import StepPassword from "../pages/MultiStepForm/StepPassword/StepPassword";
+import StepUsername from "../pages/MultiStepForm/StepUsername/StepUsername";
+import StepProfilePicture from "../pages/MultiStepForm/StepProfilePicture/StepProfilePicture";
+
+const initialState = {
+  currentStep: 1,
+
+  steps: {
+    1: StepContactDetails,
+    2: StepUsername,
+    3: StepPassword,
+    4: StepProfilePicture,
+  },
+};
+
+export const multiStepFormSlice = createSlice({
+  name: "multiStepForm",
+  initialState,
+  reducers: {
+    setStep: (state, action) => {
+      state.currentStep += 1;
+    },
+  },
+});
+
+export const { setStep } = multiStepFormSlice.actions;
+
+export default multiStepFormSlice.reducer;
