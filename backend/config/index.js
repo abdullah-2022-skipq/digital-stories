6e614ecf58d0f1118491af914c7ea3e01924fcad
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-02 16:21:26 +0500

    update env variables

--- backend/config/index.js
+++ backend/config/index.js
@@ -2,4 +2,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const { PORT, DEBUG_MODE, MONGO_DB_LOCAL, TOKEN_SECRET } = process.env;
+export const {
+  PORT,
+  DEBUG_MODE,
+  MONGO_DB_LOCAL,
+  ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET,
+  BASE_URL,
+} = process.env;
