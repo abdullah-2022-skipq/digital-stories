2d5b1b3da2e9fc68dfc9f10b693f1a7d2c6a12a8
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-02 22:53:13 +0500

    (debugging) add button to test jwt validity

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { getCurrentUser } from "../../api";
 
 const Home = () => {
-  return <div>Home Page - home to all the stories</div>;
+  const testClick = async () => {
+    const response = await getCurrentUser();
+    console.log(response);
+  };
+  return (
+    <>
+      <div>Home Page - home to all the stories</div>
+      <button onClick={testClick}>press me</button>
+    </>
+  );
 };
 
 export default Home;
