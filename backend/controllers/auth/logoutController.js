7c2c74ca2a8331081f960ee03aa9b0fc0f95d94d
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-03 23:27:34 +0500

    add logout controller

---
+++ backend/controllers/auth/logoutController.js
@@ -0,0 +1,23 @@
+import { RefreshToken } from "../../models";
+
+const logoutController = {
+  async logout(req, res, next) {
+    try {
+      // delete the token from db
+
+      const { refreshToken } = req.cookies;
+
+      await RefreshToken.deleteOne({ refreshToken });
+
+      // delete cookies
+      res.clearCookie("refreshToken");
+      res.clearCookie("accessToken");
+
+      res.json({ user: null, false: null });
+    } catch (error) {
+      next(error);
+    }
+  },
+};
+
+export default logoutController;
--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -2,10 +2,12 @@ import registerController from "./auth/registerController";
 import loginController from "./auth/loginController";
 import userController from "./user/userController";
 import refreshController from "./auth/refreshController";
+import logoutController from "./auth/logoutController";
 
 export {
   registerController,
   loginController,
   userController,
   refreshController,
+  logoutController,
 };
