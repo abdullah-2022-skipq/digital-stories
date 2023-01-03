a56fb761bd1716ee70e694727280685857c8eaa8
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-03 23:28:30 +0500

    add custom hook for persistent sessions

---
+++ frontend/src/hooks/usePersistentSession.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setAuth } from "../store/authSlice";
+
+export const usePersistentSession = () => {
+  const [loading, setLoading] = useState(false);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await axios.get(
+          `${import.meta.env.VITE_REACT_APP_API_PATH}/api/refresh`,
+          { withCredentials: true }
+        );
+
+        dispatch(setAuth());
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
+
+  return { loading };
+};
