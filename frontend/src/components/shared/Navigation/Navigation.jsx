d4e41197bd85f05a1b917ebd21b260845867415e
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-04 22:39:55 +0500

    update navbar

--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import styles from "./Navigation.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../../api";
+import { setAuth } from "../../../store/authSlice";
 
 const Navigation = () => {
   const logoStyle = {
@@ -20,12 +23,39 @@ const Navigation = () => {
     flexShrink: 0,
   };
 
+  const dispatch = useDispatch();
+
+  const logoutHandler = async () => {
+    const response = await logout();
+    dispatch(setAuth(response.data));
+  };
+
+  const { isAuth } = useSelector((state) => state.auth);
+  const user = useSelector((state) => state.user);
+
   return (
     <nav className={`${styles.navbar} container`}>
       <Link style={logoStyle} to="/">
         {/* <img style={logoImg} src="/images/navbar_snap.png" alt="logo" /> */}
         <span style={logoText}>Digital Stories</span>
       </Link>
+
+      {isAuth && (
+        <div className={styles.navRight}>
+          <Link to="/">
+            <img
+              className={styles.avatar}
+              src={user.avatar}
+              alt="avatar"
+              width={30}
+              height={30}
+            />
+          </Link>
+          <button className={styles.logoutButton} onClick={logoutHandler}>
+            <img src="/images/log_out.png" alt="logout" />
+          </button>
+        </div>
+      )}
     </nav>
   );
 };
