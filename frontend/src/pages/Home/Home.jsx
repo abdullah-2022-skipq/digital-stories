04c7fde88cc9724805ce42f0017ca032574201f1
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-04 22:42:17 +0500

    refactor

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,27 +1,10 @@
 import React from "react";
 import { getCurrentUser, logout } from "../../api";
-import { setAuth } from "../../store/authSlice";
-import { useDispatch } from "react-redux";
 
 const Home = () => {
-  const testClick = async () => {
-    const response = await getCurrentUser();
-    // console.log(response);
-  };
-
-  const dispatch = useDispatch();
-
-  const logoutHandler = async () => {
-    const response = await logout();
-    // console.log(response);
-    dispatch(setAuth(response.data.auth));
-  };
-
   return (
     <>
-      <div>Home Page - home to all the stories</div>
-      <button onClick={testClick}>press me</button>
-      <button onClick={logoutHandler}>Log Out</button>
+      <h1>sdsads</h1>
     </>
   );
 };
