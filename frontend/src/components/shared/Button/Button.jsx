3ad9bce89ff002e14bc566e3a43235f650ef02f4
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-06 23:06:09 +0500

    make button component dynamic

--- frontend/src/components/shared/Button/Button.jsx
+++ frontend/src/components/shared/Button/Button.jsx
@@ -3,10 +3,10 @@ import styles from "./Button.module.css";
 const Button = (props) => {
   return (
     <button onClick={props.onClick} className={styles.button} {...props}>
-      <span>{props.buttontitle}</span>
+      <span>{props.buttonTitle}</span>
       <img
-        className={styles.arrow}
-        src="/images/arrow_right.png"
+        className={styles.btnImg}
+        src={`/images/${props.buttonImage}.png` || "/images/arrow_right.png"}
         alt="right arrow"
       />
     </button>
