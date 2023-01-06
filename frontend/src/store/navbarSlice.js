e366efbbfa707890e8d772488e54a018ffc0b0e5
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-07 01:39:24 +0500

    add navbar slice

---
+++ frontend/src/store/navbarSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  activeRoute: "home",
+};
+
+export const navbarSlice = createSlice({
+  name: "navbar",
+  initialState,
+  reducers: {
+    setActiveRoute: (state, action) => {
+      state.activeRoute = action.payload;
+    },
+  },
+});
+
+export const { setActiveRoute } = navbarSlice.actions;
+
+export default navbarSlice.reducer;
