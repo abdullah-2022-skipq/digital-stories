0243daa85f525241511ab5425dac95d09ef7097c
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-08 02:58:06 +0500

    refactor - context api

--- frontend/src/pages/MultiStepCreateStoryForm/MultiStepCreatStoryEntry.jsx
+++ frontend/src/pages/MultiStepCreateStoryForm/MultiStepCreatStoryEntry.jsx
@@ -1,19 +1,9 @@
-import { useState } from "react";
-import StepChooseMediaType from "./StepChooseMediaType/StepChooseMediaType";
-import StepStoryContent from "./StepStoryContent/StepStoryContent";
+import { useContext } from "react";
+import { globalContext } from "../../App";
 
 const MultiStepCreateStoryEntry = () => {
-  const steps = {
-    1: StepChooseMediaType,
-    2: StepStoryContent,
-  };
-
-  const [step, setStep] = useState(1);
+  const { steps, step, onNextHandler } = useContext(globalContext);
   const CurrentStep = steps[step];
-
-  const onNextHandler = () => {
-    setStep(step + 1);
-  };
 
   return <CurrentStep onNext={onNextHandler} />;
 };
