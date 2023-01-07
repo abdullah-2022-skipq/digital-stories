8d85d2cee68fa30394fa15136896d33ee7b54dd1
Abdullah <abdullah.tahir.skipq@gmail.com>
2023-01-07 22:33:51 +0500

    update svg color

--- frontend/src/components/shared/Spinner/Spinner.jsx
+++ frontend/src/components/shared/Spinner/Spinner.jsx
@@ -9,18 +9,25 @@ const Spinner = ({ message }) => {
         <svg
           className={styles.spinner}
           xmlns="http://www.w3.org/2000/svg"
-          width="100"
-          height="100"
+          width="138"
+          height="138"
           fill="none"
         >
-          <circle cx="50" cy="50" r="45.5" stroke="#BEBCC0" strokeWidth="9" />
+          <circle
+            cx="69"
+            cy="69"
+            r="45.5"
+            stroke="#BEBCC0"
+            stroke-width="9"
+            transform="rotate(57.792 69 69)"
+          />
           <mask id="a" fill="#fff">
-            <path d="M100 50a50 50 0 1 1-14.645-35.355L79.02 20.98A41.04 41.04 0 1 0 91.04 50H100Z" />
+            <path d="M95.65 111.306a50.004 50.004 0 0 1-62.944-7.915 49.999 49.999 0 1 1 85.053-23.32l-8.737-1.984A41.042 41.042 0 0 0 58.147 29.42a41.04 41.04 0 1 0 32.728 74.305l4.775 7.581Z" />
           </mask>
           <path
-            stroke="#33B357"
-            strokeWidth="18"
-            d="M100 50a50 50 0 1 1-14.645-35.355L79.02 20.98A41.04 41.04 0 1 0 91.04 50H100Z"
+            stroke="#07F"
+            stroke-width="18"
+            d="M95.65 111.306a50.004 50.004 0 0 1-62.944-7.915 49.999 49.999 0 1 1 85.053-23.32l-8.737-1.984A41.042 41.042 0 0 0 58.147 29.42a41.04 41.04 0 1 0 32.728 74.305l4.775 7.581Z"
             mask="url(#a)"
           />
         </svg>
