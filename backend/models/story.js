08c7e3730630c6966c0c41ffb52d295c43bb14e8
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-09 17:05:42 +0500

    update story model

--- backend/models/story.js
+++ backend/models/story.js
@@ -4,15 +4,16 @@ const Schema = mongoose.Schema;
 
 const storySchema = new Schema(
   {
-    caption: { type: String, default: "" },
+    mediaType: { type: String, required: true },
+    font: { type: String },
+    fontColor: { type: String },
+    caption: { type: String },
     image: { type: String, default: "" },
     video: { type: String, default: "" },
-    bgColor: { type: Number, required: true, default: 0 },
-    font: { type: Number, required: true, default: 0 },
     upVoteCount: { type: Number, default: 0 },
     downVoteCount: { type: Number, default: 0 },
     commentCount: { type: Number, default: 0 },
-    postedBy: { type: mongoose.SchemaTypes.ObjectId, ref: "users" },
+    postedBy: { type: mongoose.SchemaTypes.ObjectId, ref: "User" },
   },
   { timestamps: true }
 );
