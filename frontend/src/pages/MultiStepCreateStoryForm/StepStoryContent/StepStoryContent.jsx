1840b389dbc56ea8322dc9f3fa9bb869d3cfad84
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-09 17:09:21 +0500

    update

--- frontend/src/pages/MultiStepCreateStoryForm/StepStoryContent/StepStoryContent.jsx
+++ frontend/src/pages/MultiStepCreateStoryForm/StepStoryContent/StepStoryContent.jsx
@@ -1,14 +1,12 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import Card from "../../../components/shared/Card/Card";
 import styles from "./StepStoryContent.module.css";
-import Button from "../../../components/shared/Button/Button";
 import TextStory from "../../../components/TextStory/TextStory";
 import { globalContext } from "../../../App";
 import ImageStory from "../../../components/ImageStory/ImageStory";
+import VideoStory from "../../../components/VideoStory/VideoStory";
 
 const StepStoryContent = () => {
-  const [hover, setHover] = useState(false);
-
   const { mediaType, onPrevHandler } = useContext(globalContext);
 
   return (
@@ -25,15 +23,7 @@ const StepStoryContent = () => {
           <Card cardHeading="Create a story" cardLogo="create-story">
             {mediaType == "Text" && <TextStory />}
             {mediaType == "Image" && <ImageStory />}
-            {mediaType == "Video" && ";s;s"}
-            <Button
-              buttontitle="Post"
-              buttonimage="party_popper"
-              onClick={() => {}}
-              onMouseEnter={() => setHover(true)}
-              onMouseLeave={() => setHover(false)}
-              style={{ backgroundColor: hover ? "#0f6632" : "#20BD5F" }}
-            />
+            {mediaType == "Video" && <VideoStory />}
           </Card>
         </div>
       </div>
