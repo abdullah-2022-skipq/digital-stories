a7dbe907e2368ad737e3c2464482b3ecb71a27c6
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-09 17:07:15 +0500

    fix syntax

--- frontend/src/components/shared/Spinner/Spinner.jsx
+++ frontend/src/components/shared/Spinner/Spinner.jsx
@@ -18,7 +18,7 @@ const Spinner = ({ message }) => {
             cy="69"
             r="45.5"
             stroke="#BEBCC0"
-            stroke-width="9"
+            strokeWidth="9"
             transform="rotate(57.792 69 69)"
           />
           <mask id="a" fill="#fff">
@@ -26,7 +26,7 @@ const Spinner = ({ message }) => {
           </mask>
           <path
             stroke="#07F"
-            stroke-width="18"
+            strokeWidth="18"
             d="M95.65 111.306a50.004 50.004 0 0 1-62.944-7.915 49.999 49.999 0 1 1 85.053-23.32l-8.737-1.984A41.042 41.042 0 0 0 58.147 29.42a41.04 41.04 0 1 0 32.728 74.305l4.775 7.581Z"
             mask="url(#a)"
           />
