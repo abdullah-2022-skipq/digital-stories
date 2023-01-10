8b3b999549ad6b007c58fed99e363cb4c808b033
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-11 00:55:12 +0500

    -

--- frontend/src/components/Comments/Comment.jsx
+++ frontend/src/components/Comments/Comment.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./Comment.module.css";
 
 const Comment = ({ comment }) => {
-  console.log(comment);
   return (
     <div className={styles.comment}>
       <div className={styles.avatarWrapper}>
