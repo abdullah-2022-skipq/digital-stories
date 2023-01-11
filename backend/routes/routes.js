0732cdb6a67b4a361f13d0a9a25292e568385cd3
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-11 22:35:51 +0500

    add routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -26,7 +26,8 @@ router.get("/stories/trending", auth, storyController.getTrending);
 router.get("/stories/:id", auth, storyController.getById);
 router.post("/comment", auth, commentController.createComment);
 router.get("/comments/:id", auth, commentController.getCommentsByPostId);
-router.post("/like", auth, votesController.upVote);
+router.post("/upvote", auth, votesController.upVote);
+router.post("/downvote", auth, votesController.downVote);
 router.get("/engagements", auth, engagementController.getEngagements);
 
 export default router;
