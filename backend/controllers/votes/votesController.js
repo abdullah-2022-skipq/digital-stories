023b6326ef8c88f7d7f100009bd40fc0ec51b3ab
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-11 22:35:22 +0500

    add controller method

--- backend/controllers/votes/votesController.js
+++ backend/controllers/votes/votesController.js
@@ -11,11 +11,11 @@ const votesController = {
          -> if yes then delete dislike record and add like record
         if no then add like record
 
-        in any case, update story collection accordingly
+        in any case, update story and engagement collection accordingly
 
         */
 
-    const likeSchema = Joi.object({
+    const upvoteSchema = Joi.object({
       user: Joi.string()
         .regex(/^[0-9a-fA-F]{24}$/)
         .required(),
@@ -24,7 +24,7 @@ const votesController = {
         .required(),
     });
 
-    const { error } = likeSchema.validate(req.body);
+    const { error } = upvoteSchema.validate(req.body);
 
     if (error) {
       return next(error);
@@ -63,6 +63,7 @@ const votesController = {
       byUser: user,
       onPost: post,
     });
+
     if (downvoted) {
       await Engagement.deleteOne({
         action: "downvote",
@@ -73,7 +74,7 @@ const votesController = {
       // update story
       await Story.findOneAndUpdate(
         { _id: post },
-        { $inc: { downVoteCount: -1 } },
+        { $inc: { downVoteCount: -1, upVoteCount: 1 } },
         { new: true }
       );
 
@@ -90,16 +91,105 @@ const votesController = {
     // return;
 
     // if not both then add record for upvote
-    const newLike = new Engagement({
+    const newUpvote = new Engagement({
       action: "upvote",
       onPost: post,
       byUser: user,
       forUser: story.postedBy,
     });
 
-    await newLike.save();
+    await newUpvote.save();
 
     return res.status(200).json({ message: "nothing before, now +1" });
+  },
+
+  async downVote(req, res, next) {
+    const downVoteSchema = Joi.object({
+      user: Joi.string()
+        .regex(/^[0-9a-fA-F]{24}$/)
+        .required(),
+      post: Joi.string()
+        .regex(/^[0-9a-fA-F]{24}$/)
+        .required(),
+    });
+
+    const { error } = downVoteSchema.validate(req.body);
+
+    if (error) {
+      return next(error);
+    }
+
+    const { post, user } = req.body;
+
+    // check already disliked
+    const downvoted = await Engagement.findOne({
+      action: "downvote",
+      byUser: user,
+      onPost: post,
+    });
+
+    if (downvoted) {
+      // delete record
+      await Engagement.deleteOne({
+        action: "downvote",
+        byUser: user,
+        onPost: post,
+      });
+
+      // update story
+      await Story.findOneAndUpdate(
+        { _id: post },
+        { $inc: { downVoteCount: -1 } },
+        { new: true }
+      );
+
+      return res.status(200).json({ message: "downvoted before, now -1" });
+    }
+
+    // check already liked
+    const voted = await Engagement.findOne({
+      action: "upvote",
+      byUser: user,
+      onPost: post,
+    });
+
+    if (voted) {
+      await Engagement.deleteOne({
+        action: "upvote",
+        byUser: user,
+        onPost: post,
+      });
+
+      // update story
+      await Story.findOneAndUpdate(
+        { _id: post },
+        { $inc: { upVoteCount: -1, downVoteCount: 1 } },
+        { new: true }
+      );
+
+      return res.status(200).json({ message: "upvoted before, now level" });
+    }
+
+    const story = await Story.findOneAndUpdate(
+      { _id: post },
+      { $inc: { downVoteCount: 1 } },
+      { new: true }
+    );
+
+    // console.log(story);
+    // return;
+
+    // if not both then add record for upvote
+    const newDownvote = new Engagement({
+      action: "downvote",
+      onPost: post,
+      byUser: user,
+      forUser: story.postedBy,
+    });
+
+    await newDownvote.save();
+
+    return res.status(200).json({ message: "nothing before, now -1" });
   },
 };
 export default votesController;
