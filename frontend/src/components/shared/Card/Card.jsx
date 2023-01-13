9341531f79dc2be90d2829ae08fdc503a794e29d
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-13 09:45:05 +0500

    add tests for card component

--- frontend/src/components/shared/Card/Card.jsx
+++ frontend/src/components/shared/Card/Card.jsx
@@ -2,13 +2,20 @@ import styles from './Card.module.css';
 
 function Card({ cardHeading, cardLogo, children }) {
   return (
-    <div className={styles.card}>
-      <div className={styles.headingWrapper}>
+    <div
+      className={styles.card}
+      data-testid='card'
+    >
+      <div
+        className={styles.headingWrapper}
+        data-testid='headingWrapper'
+      >
         {cardLogo && (
           <img
             className={styles.logoImg}
             src={`/images/${cardLogo}.png`}
-            alt="card-logo"
+            alt='card-logo'
+            data-testid='cardLogo'
           />
         )}
         {cardHeading && <h1 className={styles.heading}>{cardHeading}</h1>}
---
+++ frontend/src/components/shared/Card/Card.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Card from "./Card";
+
+describe("Card component", () => {
+  test("renders card properly", () => {
+    render(<Card />);
+  });
+
+  test("has the correct local css class in outer div", () => {
+    const { getByTestId } = render(<Card />);
+    const outerDiv = getByTestId("card");
+    expect(outerDiv).toHaveClass("card");
+  });
+
+  test("has the correct local css class in inner div", () => {
+    const { getByTestId } = render(<Card />);
+    const innerDiv = getByTestId("headingWrapper");
+    expect(innerDiv).toHaveClass("headingWrapper");
+  });
+
+  test("renders the card heading", () => {
+    const cardHeading = "Card Heading";
+    const { getByText } = render(<Card cardHeading={cardHeading} />);
+    const heading = getByText(cardHeading);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("renders the card logo", () => {
+    const cardLogo = "cardLogo.png";
+    const { getByTestId } = render(<Card cardLogo={cardLogo} />);
+    const imgElement = getByTestId("cardLogo");
+    expect(imgElement).toHaveAttribute("src", `/images/${cardLogo}.png`);
+  });
+
+  test("renders children", () => {
+    const children = <p>Children for test</p>;
+    const { getByText } = render(<Card>{children}</Card>);
+    const childrenElement = getByText(/Children for test/i);
+    expect(childrenElement).toBeInTheDocument();
+  });
+});
