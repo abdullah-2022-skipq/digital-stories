efa1191458ea80f98b9250c2df6631170a99025a
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-14 00:14:36 +0500

    add vote-status api route

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 import {
   registerController,
   loginController,
@@ -10,27 +10,28 @@ import {
   votesController,
   engagementController,
   leaderboardController,
-} from "../controllers";
+} from '../controllers';
 
-import { auth } from "../middlewares";
+import { auth } from '../middlewares';
 
 const router = express.Router();
 
-router.post("/register", registerController.register);
-router.post("/login", loginController.login);
-router.get("/me", auth, userController.myDetails);
-router.get("/refresh", refreshController.refresh);
-router.post("/logout", auth, logoutController.logout);
-router.get("/stories", auth, storyController.getAll);
-router.post("/stories", auth, storyController.create);
-router.get("/stories/trending", auth, storyController.getTrending);
-router.get("/stories/:id", auth, storyController.getById);
-router.delete("/stories/:id", auth, storyController.deleteById);
-router.post("/comment", auth, commentController.createComment);
-router.get("/comments/:id", auth, commentController.getCommentsByPostId);
-router.post("/upvote", auth, votesController.upVote);
-router.post("/downvote", auth, votesController.downVote);
-router.get("/engagements", auth, engagementController.getEngagements);
-router.get("/leaderboard", auth, leaderboardController.getLeaderboard);
+router.post('/register', registerController.register);
+router.post('/login', loginController.login);
+router.get('/me', auth, userController.myDetails);
+router.get('/refresh', refreshController.refresh);
+router.post('/logout', auth, logoutController.logout);
+router.get('/stories', auth, storyController.getAll);
+router.post('/stories', auth, storyController.create);
+router.get('/stories/trending', auth, storyController.getTrending);
+router.get('/stories/:id', auth, storyController.getById);
+router.delete('/stories/:id', auth, storyController.deleteById);
+router.post('/comment', auth, commentController.createComment);
+router.get('/comments/:id', auth, commentController.getCommentsByPostId);
+router.post('/upvote', auth, votesController.upVote);
+router.post('/downvote', auth, votesController.downVote);
+router.get('/engagements', auth, engagementController.getEngagements);
+router.get('/leaderboard', auth, leaderboardController.getLeaderboard);
+router.post('/vote-status', auth, votesController.getVoteStatus);
 
 export default router;
