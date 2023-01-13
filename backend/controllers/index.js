c4c84c213d8015f9756e11f0012c07bb11743495
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-13 15:29:31 +0500

    add static code analysis

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,12 +1,13 @@
-import registerController from './auth/registerController';
-import loginController from './auth/loginController';
-import userController from './user/userController';
-import refreshController from './auth/refreshController';
-import logoutController from './auth/logoutController';
-import storyController from './story/storyController';
-import commentController from './comments/commentController';
-import votesController from './votes/votesController';
-import engagementController from './engagements/engagementController';
+import registerController from "./auth/registerController";
+import loginController from "./auth/loginController";
+import userController from "./user/userController";
+import refreshController from "./auth/refreshController";
+import logoutController from "./auth/logoutController";
+import storyController from "./story/storyController";
+import commentController from "./comments/commentController";
+import votesController from "./votes/votesController";
+import engagementController from "./engagements/engagementController";
+import leaderboardController from "./leaderboard/leaderboardController";
 
 export {
   registerController,
@@ -18,4 +19,5 @@ export {
   commentController,
   votesController,
   engagementController,
+  leaderboardController,
 };
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import {
   registerController,
   loginController,
@@ -9,26 +9,28 @@ import {
   commentController,
   votesController,
   engagementController,
-} from '../controllers';
+  leaderboardController,
+} from "../controllers";
 
-import { auth } from '../middlewares';
+import { auth } from "../middlewares";
 
 const router = express.Router();
 
-router.post('/register', registerController.register);
-router.post('/login', loginController.login);
-router.get('/me', auth, userController.myDetails);
-router.get('/refresh', refreshController.refresh);
-router.post('/logout', auth, logoutController.logout);
-router.get('/stories', auth, storyController.getAll);
-router.post('/stories', auth, storyController.create);
-router.get('/stories/trending', auth, storyController.getTrending);
-router.get('/stories/:id', auth, storyController.getById);
-router.delete('/stories/:id', auth, storyController.deleteById);
-router.post('/comment', auth, commentController.createComment);
-router.get('/comments/:id', auth, commentController.getCommentsByPostId);
-router.post('/upvote', auth, votesController.upVote);
-router.post('/downvote', auth, votesController.downVote);
-router.get('/engagements', auth, engagementController.getEngagements);
+router.post("/register", registerController.register);
+router.post("/login", loginController.login);
+router.get("/me", auth, userController.myDetails);
+router.get("/refresh", refreshController.refresh);
+router.post("/logout", auth, logoutController.logout);
+router.get("/stories", auth, storyController.getAll);
+router.post("/stories", auth, storyController.create);
+router.get("/stories/trending", auth, storyController.getTrending);
+router.get("/stories/:id", auth, storyController.getById);
+router.delete("/stories/:id", auth, storyController.deleteById);
+router.post("/comment", auth, commentController.createComment);
+router.get("/comments/:id", auth, commentController.getCommentsByPostId);
+router.post("/upvote", auth, votesController.upVote);
+router.post("/downvote", auth, votesController.downVote);
+router.get("/engagements", auth, engagementController.getEngagements);
+router.get("/leaderboard", auth, leaderboardController.getLeaderboard);
 
 export default router;
