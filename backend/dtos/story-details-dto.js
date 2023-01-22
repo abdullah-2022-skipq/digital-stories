e2dbf2dfa5f420d02e96fd89e91e8028b38c5c4c
Abdullah <111970567+abdullah-2022-skipq@users.noreply.github.com>
2023-01-22 22:07:27 +0500

    update dtos for access mode

--- backend/dtos/story-details-dto.js
+++ backend/dtos/story-details-dto.js
@@ -14,6 +14,7 @@ class StoryDetailsDTO {
     this.commentCount = story.commentCount;
     this.username = story.postedBy.username;
     this.postedBy = story.postedBy._id;
+    this.isPrivate = story.isPrivate;
   }
 }
 
